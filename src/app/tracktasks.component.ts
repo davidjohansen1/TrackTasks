94c516a66f6e9efab29e4e68b3432378dd626c63
David Johansen <davidjohansen1@mail.weber.edu>
2021-02-02 18:38:57 -0700

    Creating the login page

---
+++ src/app/tracktasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tracktasks',
+  templateUrl: './tracktasks.component.html',
+  styleUrls: ['./tracktasks.component.css']
+})
+export class TrackTasksComponent {
+  title = 'tracktasks';
+}
